8c58008e6c369cd72034aee0329fb80d25b09230
Adrian Zaręba <dev.adrianzareba@gmail.com>
2021-05-13 15:31:14 +0200

    Initial commit

---
+++ SharpWebServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharpWebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
